ed386ccb0c72b273d0c5c20f75634e7f09147eda
ilona <32567245+IlonaBocancea@users.noreply.github.com>
2019-11-24 23:00:56 +0200

    Add files via upload

---
+++ 1/Untitled2.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+int main ()
+{ int a,b,c,max;
+   cout<<"a="; cin>>a;
+   cout<<"b="; cin>>b;
+   if (a>b>c) max=a;
+   else max=b;
+   if (b<c) max=c
+   cout<<"maximul este "<<max;
+return 0;
+}
+
